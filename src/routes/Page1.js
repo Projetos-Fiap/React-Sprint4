fc8ff154b01104001df719c6611c6a6fe67fe87b
andrelambert <rm99148@fiap.com.br>
2023-09-11 20:12:35 -0300

    adicionando google maps

--- src/routes/Page1.js
+++ src/routes/Page1.js
@@ -1,10 +1,16 @@
 import React from "react";
 
-
 function Page1() {
   return (
     <div className="page1">
-      <h1>Pontos de coleta</h1>
+      <div className="container">
+        <h2 className="title">Pontos de coleta</h2>
+        <iframe
+          title="Google Maps"
+          src="https://www.google.com/maps/d/embed?mid=1H2xZ0GS-A_vT9nZw9JhPo-ZaCzU&ehbc=2E312F"
+          width="640" height="480">
+        </iframe>
+      </div>  
     </div>
   );
 }
