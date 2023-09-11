2b11bbab323c849eaac495926bcf4feea7285143
andrelambert <rm99148@fiap.com.br>
2023-09-11 20:20:04 -0300

    texto na página de mapa

--- src/routes/Page1.js
+++ src/routes/Page1.js
@@ -5,6 +5,7 @@ function Page1() {
     <div className="page1">
       <div className="container">
         <h2 className="title">Pontos de coleta</h2>
+        <p className="title">Encontre o ponto de reciclagem mais próximo de você</p>
         <iframe
           title="Google Maps"
           src="https://www.google.com/maps/d/embed?mid=1H2xZ0GS-A_vT9nZw9JhPo-ZaCzU&ehbc=2E312F"
