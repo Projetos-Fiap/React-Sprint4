9e16631a2741a833fbcc75551d07c37a23776342
andrelambert <rm99148@fiap.com.br>
2023-09-12 20:13:16 -0300

    styled components da página de erro

--- src/routes/ErrorPage.js
+++ src/routes/ErrorPage.js
@@ -5,12 +5,23 @@ const ErrorPageContainer = styled.div`
   text-align: center;
 `;
 
+const ErrorPageTitle = styled.h2`
+  font-size: 2.5rem;
+  color: #263243;
+  margin-bottom: 0.5em;
+`;
+
+const ErrorPageText = styled.p`
+  font-size: 1.5rem;
+  margin-bottom: 1em; 
+`;
+
 function ErrorPage() {
   return (
     <div className="errorpage">
       <ErrorPageContainer>
-        <h2>Oh não!</h2>
-        <p>Não achamos o que você estava procurando.</p>
+        <ErrorPageTitle>Oh não!</ErrorPageTitle>
+        <ErrorPageText>Não achamos o que você estava procurando.</ErrorPageText>
       </ErrorPageContainer> 
     </div>
   );
