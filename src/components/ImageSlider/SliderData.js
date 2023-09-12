036ada09ac91574cdb2f3535dbb598eeec3a98a5
andrelambert <rm99148@fiap.com.br>
2023-09-12 18:00:43 -0300

    slider v1

---
+++ src/components/ImageSlider/SliderData.js
@@ -0,0 +1,33 @@
+import HomeSlide1 from './images/HomeSlide1.jpg';
+import HomeSlide2 from './images/HomeSlide2.jpg';
+import HomeSlide3 from './images/HomeSlide3.jpg';
+import HomeSlide4 from './images/HomeSlide4.jpg';
+import HomeSlide5 from './images/HomeSlide5.jpg';
+
+export const sliderData = [
+    {
+      image: HomeSlide1,
+      heading: "Slide One",
+      desc: "This is the description of slide one Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.",
+    },
+    {
+      image: HomeSlide2,
+      heading: "Slide Two",
+      desc: "This is the description of slide two Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.",
+    },
+    {
+      image: HomeSlide3,
+      heading: "Slide Three",
+      desc: "This is the description of slide three Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.",
+    },
+    {
+      image: HomeSlide4,
+      heading: "Slide Four",
+      desc: "This is the description of slide three Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.",
+    },
+    {
+      image: HomeSlide5,
+      heading: "Slide Five",
+      desc: "This is the description of slide three Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.",
+    },
+  ];
