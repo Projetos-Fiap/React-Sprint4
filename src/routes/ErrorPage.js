7bff52f2bc6bd7b4da76e4ab0d8458a318d5e13a
andrelambert <rm99148@fiap.com.br>
2023-09-12 20:11:34 -0300

    styled components da página de erro

--- src/routes/ErrorPage.js
+++ src/routes/ErrorPage.js
@@ -1,12 +1,17 @@
 import React from "react";
+import styled from "styled-components";
+
+const ErrorPageContainer = styled.div`
+  text-align: center;
+`;
 
 function ErrorPage() {
   return (
     <div className="errorpage">
-      <div className="container">
+      <ErrorPageContainer>
         <h2>Oh não!</h2>
         <p>Não achamos o que você estava procurando.</p>
-      </div>  
+      </ErrorPageContainer> 
     </div>
   );
 }
