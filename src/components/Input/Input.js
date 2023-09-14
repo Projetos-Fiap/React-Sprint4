677c24fd8cf29f01a6228c3c3fc2d8b0ff72b681
andrelambert <rm99148@fiap.com.br>
2023-09-14 11:31:59 -0300

    removendo teste.txt e arrumando estilo função input

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,9 +1,20 @@
 import React from "react";
-import * as C from "./Styles";
+import styled from "styled-components";
+
+const InputStyle = styled.input`
+  outline: none;
+  padding: 16px 20px;
+  width: 100%;
+  border-radius: 5px;
+  font-size: 16px;
+
+  background-color: #f0f2f5;
+  border: none;
+`;
 
 const Input = ({ type, placeholder, value, onChange }) => {
   return (
-    <C.Input
+    <InputStyle
       value={value}
       onChange={onChange}
       type={type}
