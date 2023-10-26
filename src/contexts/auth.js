d185d37b8c5d4d0a9caae25ef1afbde995ac77ec
andrelambert <rm99148@fiap.com.br>
2023-10-26 17:47:48 -0300

    início sprint 4, criando users.json

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,4 +1,5 @@
 import { createContext, useEffect, useState } from "react";
+import usersData from "../data/users.json"; // Importe os dados de usuário do arquivo JSON.
 
 export const AuthContext = createContext({});
 
@@ -7,27 +8,24 @@ export const AuthProvider = ({ children }) => {
 
   useEffect(() => {
     const userToken = localStorage.getItem("user_token");
-    const usersStorage = localStorage.getItem("users_bd");
 
-    if (userToken && usersStorage) {
-      const hasUser = JSON.parse(usersStorage)?.filter(
-        (user) => user.email === JSON.parse(userToken).email
+    if (userToken) {
+      const storedUser = usersData.users.find(
+        (userData) => userData.email === JSON.parse(userToken).email
       );
 
-      if (hasUser) setUser(hasUser[0]);
+      if (storedUser) setUser(storedUser);
     }
   }, []);
 
   const signin = (email, password) => {
-    const usersStorage = JSON.parse(localStorage.getItem("users_bd"));
+    const storedUser = usersData.users.find((userData) => userData.email === email);
 
-    const hasUser = usersStorage?.filter((user) => user.email === email);
-
-    if (hasUser?.length) {
-      if (hasUser[0].email === email && hasUser[0].password === password) {
+    if (storedUser) {
+      if (storedUser.password === password) {
         const token = Math.random().toString(36).substring(2);
         localStorage.setItem("user_token", JSON.stringify({ email, token }));
-        setUser({ email, password });
+        setUser(storedUser);
         return;
       } else {
         return "E-mail ou senha incorretos";
@@ -38,23 +36,18 @@ export const AuthProvider = ({ children }) => {
   };
 
   const signup = (email, password) => {
-    const usersStorage = JSON.parse(localStorage.getItem("users_bd"));
+    const storedUser = usersData.users.find((userData) => userData.email === email);
 
-    const hasUser = usersStorage?.filter((user) => user.email === email);
-
-    if (hasUser?.length) {
+    if (storedUser) {
       return "Já tem uma conta com esse E-mail";
     }
 
-    let newUser;
+    const newUser = { email, password };
+    usersData.users.push(newUser);
 
-    if (usersStorage) {
-      newUser = [...usersStorage, { email, password }];
-    } else {
-      newUser = [{ email, password }];
-    }
-
-    localStorage.setItem("users_bd", JSON.stringify(newUser));
+    // Atualize o arquivo JSON com o novo usuário.
+    // Certifique-se de que essa ação possa ser realizada no ambiente de desenvolvimento.
+    // Em um ambiente de produção, você usaria um servidor e um banco de dados.
 
     return;
   };
